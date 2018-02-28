130e0a12b23f1439896d47c85209065142126633
tristan <tristanp2@hotmail.com>
2018-02-27 23:30:59 -0800

    a-star seems to be working

---
+++ app/search.py
@@ -0,0 +1,115 @@
+import math
+from queue import PriorityQueue
+
+
+def in_dict(dictionary, key):
+    try:
+        dictionary[key]
+    except KeyError:
+        return False
+    else:
+        return True
+
+class AStar:
+    def __init__(self, size, pos, obstacles):
+        self.board_size = size;
+        print(self.board_size)
+        self.pos = pos;
+        self.obstacles = obstacles
+    
+    def search(self, goal):
+        evaluated = {}
+
+        open_set_pq = PriorityQueue()
+        open_set_pq.put((0, self.pos)) 
+        open_set = {}
+        open_set[self.pos] = 0
+        #cost of getting to these nodes from start
+        cost_from_start = {}
+        cost_from_start[self.pos] = 0
+        cost_to_goal = {}
+        came_from = {}
+        
+
+        for x in range(self.board_size):
+            for y in range(self.board_size):
+                pos = (x,y)
+                cost_from_start[pos] = math.inf
+                came_from[pos] = None
+                cost_to_goal[pos] = math.inf
+
+        cost_to_goal[self.pos] = self.heuristic(self.pos, goal)
+        #print("finding path {} --> {}".format(self.pos,goal))
+        i = 0
+        while not open_set_pq.empty():
+            i += 1
+            (current_score, current_pos) = open_set_pq.get()
+
+            #print("current: ", current_pos)
+            if current_pos == goal:
+                print("Num_iterations: ",i)
+                return self.reconstruct_path(came_from, current_pos)
+             
+            evaluated[current_pos] = current_score
+            neighbours = self.get_neighbours(current_pos)
+            #print("searching neighbours")
+            for neighbour in neighbours:
+                if in_dict(evaluated,neighbour):
+                    continue
+                elif in_dict(self.obstacles,neighbour):
+                    continue
+                elif not in_dict(cost_to_goal,neighbour):
+                    continue
+
+                if not in_dict(open_set, neighbour):
+                    open_set[neighbour] = math.inf
+
+                tentative_score = cost_to_goal[current_pos] + 1
+                if tentative_score > cost_to_goal[neighbour]:
+                    open_set_pq.put((cost_from_start[neighbour],neighbour))
+                    continue
+
+                came_from[neighbour] = current_pos
+                cost_to_goal[neighbour] = tentative_score
+                cost_from_start[neighbour] = tentative_score + self.heuristic(neighbour,goal)
+                open_set_pq.put((cost_from_start[neighbour],neighbour))
+
+
+    def reconstruct_path(self, came_from, current):
+        total_path = [current]
+        while True:
+            current = came_from[current]
+            if(current == None):
+                break
+            total_path.append(current)
+        return total_path
+
+        
+    def get_neighbours(self, pos):
+        (x,y) = pos
+        x0 = x-1
+        x1 = x+1
+        y0 = y-1
+        y1 = y+1
+        if x0 < 0:
+            x0=0
+        if y0 < 0:
+            y0=0
+        if x1 >= self.board_size:
+            x1 = self.board_size - 1
+        if y1 >= self.board_size:
+            y1 = self.board_size - 1
+
+        return [(x0,y),(x1,y),(x,y0),(x,y1)]
+
+    def heuristic(self,start, end):
+        (x1,y1) = start
+        (x2,y2) = end
+        return abs(x2-x1) + abs(y2-y1)
+
+
+if __name__=='__main__':
+    search = AStar(10,(0,0),{})
+    print(search.search((5,5)))
+
+
